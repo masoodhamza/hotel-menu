6c392c34a68af465ee422298e1a64796c63d4c77
Hamza Masood <masood.hamzaa@gmail.com>
2021-05-19 21:36:48 +0500

    theroy.js question answers

---
+++ theory.js
@@ -0,0 +1,26 @@
+/*
+What is the DOM?
+Document Object Model, is a web standard on which a website is build. 
+HTML DOM Tree's parent node is document, then html.
+under html there are two childs: head, body.
+head consists title, and meta tags etc
+body consists all working tags i.e., div, h1, p
+DOM helps us in manupulating/playing with code by javascript etc
+
+What is an event?
+every task on webpage performed is an event, 
+i.e., pageload, click, mouseover, mousedown,mouseup, keydown, keyup etc
+and much more.
+
+What is an event listener?
+event lisner is a method which helps us to control that occuring evnet and 
+perform any task which we require. i.e., on submit button click submit the form after validating etc
+
+Why would we convert a NodeList into an Array?
+yes we can convert a NodeList into an Array
+
+What is a component?
+component is just like a div, which contains certain task.
+i.e., form is a component, chatbox is another component,
+one page can have many components
+*/
